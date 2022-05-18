31b29c20ab9d14e128dc1b6570e5471f5b7e40ea
Ritesh <riteshkatukojwala@gmail.com>
2022-05-18 22:34:18 +0530

    added fns avformat_open_input, avformat_find_stream_info

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ encoding and decoding
 combining the subtitles, video, audio(inputs) into container.
 ## demuxing
 seperating container into metadata.
+## video file header
+it contains info about the video file such as it's format, size, resolution ..
--- a.c
+++ a.c
@@ -1,7 +1,29 @@
 //for encoding and decoding data (video as well as audio)
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <ffmpeg/swscale.h>
+#include "libavcodec/avcodec.h"
+//multiplexing and demultiplexing data
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
 int main(int argc,char **argv){
+    //structure pointer
+    AVFormatContext *pFormatCtx = NULL;
+    //string is a character array
+    //file name 
+    const char *filename = "test.mp4";
+    //open input container
+    //arguments - 1.avformatcontext 2.filename 3.format(if not specified,it will be autodetected) 
+    //this auto detects the format of the file, reads the header info and allocates the data to the context
+    if(avformat_open_input(&pFormatCtx,filename,NULL,NULL)<0){
+        printf("couldn't open file\n");
+        return -1;
+    }
+    //some formats don't have a header(or some data in context may be missing), so we need to call this function.
+    if(avformat_find_stream_info(pFormatCtx,NULL)<0) {
+        printf("couldn't find stream info\n");
+        return -1;
+    }
+    //print some info present in the context
+    //arguments 1.context 2.index of stream to dump info abt 3.url of file 4.whether context is input or output
+    av_dump_format(pFormatCtx,0,filename,0);
+    
 
 }
